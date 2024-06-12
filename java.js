3bb409b3824e3b95797a100eec95f29060a1f259
silvieruzzie <169399791+silvieruzzie@users.noreply.github.com>
2024-06-12 13:11:30 +0200

    Get the fuctions working

--- java.js
+++ java.js
@@ -1,6 +1,7 @@
 let numberA;
 let numberB;
 let operator;
+let result;
 let displayValue;
 
 const one = document.querySelector('.one');
@@ -17,45 +18,70 @@ const numbers = document.querySelector('.numbers');
 const operators = document.querySelector('.operators');
 const equal = document.querySelector('.equal');
 const cancel = document.querySelector('.ca');
+const point = document.querySelector('.point');
 
 //event listeners
 
 numbers.addEventListener('click', function(event) {
   if (event.target.matches('.number')) {
     console.log(event.target.textContent);
+    populate(event.target.textContent);
   }
 });
 
 operators.addEventListener('click', function(event) {
     if (event.target.matches('.operator')) {
-      console.log(event.target.textContent);
+      populate(event.target.textContent);
+      operator = event.target.textContent;
+      console.log(operator)
     }
   });
 
 equal.addEventListener('click', function(event) {
-    console.log(event.target.textContent);
+    operate(numberA, operator, numberB);
 });
 
 cancel.addEventListener('click', function(event) {
-    console.log(event.target.textContent);
+    displayValue.textContent = ' ';
 });
+
+point.addEventListener('click', function(event) {
+    console.log(event.target.textContent);
+      populate(event.target.textContent);
+})
 
 //functions
 
 function add(a, b) {
-    return a + b;
-}
+    result = a + b;
+    console.log(result);
+    populate(result);
+};
 function subtract(a,b) {
-    return a - b;
-}
+    result = a - b;
+    console.log(result);
+};
 function multiply(a,b) {
-    return a * b;
-}
+    result = a * b;
+    console.log(result);
+};
 function divide(a, b) {
-    return a / b;
-}
+    result = a / b;
+    console.log(result);
+};
 
 function populate(value) {
 displayValue = document.querySelector('.display')
-displayValue.textContent = value;
-}
+displayValue.textContent += value;
+};
+
+function operate(numberA, operator, numberB) {
+if (operator === '+') {
+    add(numberA,numberB);
+} else if (operator === '-') {
+    subtract(numberA,numberB);
+} else if (operator === '*') {
+    multiply(numberA,numberB);
+} else if (operator === '/') {
+    divide(numberA,numberB);
+}};
