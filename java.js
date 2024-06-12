3256e7e93ee1f057bf416c07e8bb9ee1262d29b1
silvieruzzie <169399791+silvieruzzie@users.noreply.github.com>
2024-06-12 11:59:50 +0200

    Add eventListener for all buttons

--- java.js
+++ java.js
@@ -1,3 +1,47 @@
+let numberA;
+let numberB;
+let operator;
+let displayValue;
+
+const one = document.querySelector('.one');
+const two = document.querySelector('.two');
+const three = document.querySelector('.three');
+const four = document.querySelector('.four');
+const five = document.querySelector('.five');
+const six = document.querySelector('.six');
+const seven = document.querySelector('.seven');
+const eight = document.querySelector('.eight');
+const nine = document.querySelector('.nine');
+const zero = document.querySelector('.zero');
+const numbers = document.querySelector('.numbers');
+const operators = document.querySelector('.operators');
+const equal = document.querySelector('.equal');
+const cancel = document.querySelector('.ca');
+
+//event listeners
+
+numbers.addEventListener('click', function(event) {
+  if (event.target.matches('.number')) {
+    console.log(event.target.textContent);
+  }
+});
+
+operators.addEventListener('click', function(event) {
+    if (event.target.matches('.operator')) {
+      console.log(event.target.textContent);
+    }
+  });
+
+equal.addEventListener('click', function(event) {
+    console.log(event.target.textContent);
+});
+
+cancel.addEventListener('click', function(event) {
+    console.log(event.target.textContent);
+});
+
+//functions
+
 function add(a, b) {
     return a + b;
 }
@@ -10,13 +54,8 @@ function multiply(a,b) {
 function divide(a, b) {
     return a / b;
 }
-let numberA;
-let numberB;
-let operator;
-let displayValue;
 
 function populate(value) {
 displayValue = document.querySelector('.display')
 displayValue.textContent = value;
 }
-
